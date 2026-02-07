80e21d07b58835a8542c8c748f98adb50d219782
Freyta <freyta@gmail.com>
2026-02-08 10:02:00 +1100

    Fix OzCruising
    OzCruising updated how their website worked when searching for cruises, so updated that.
    
    I still need to make the search simpler to use

--- Python/ozcruising/main.py
+++ Python/ozcruising/main.py
@@ -7,6 +7,7 @@ import argparse
 from mappings import SHIPS, CRUISE_LINES, PORTS, REGIONS
 from row_extractor import fetch_cruises_in_batches
 import re 
+from datetime import datetime
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -51,21 +52,22 @@ if __name__ == "__main__":
     )
 
     # General arguments
-    parser.add_argument('--origin', nargs="+", default="-111", help="Port where the cruise originates.")
-    parser.add_argument('--arrival', nargs="+", default="-111", help="Port where the cruise ends.")
-    parser.add_argument('--region', nargs="+", default="-111", help="The region you want to sail.")
-    parser.add_argument('--cruise_line', nargs="+", default="0", help="The cruise line you want to sail on.")
-    parser.add_argument('--ship', nargs="+", default="-111", help="The cruise ship you want to sail on.")
+    parser.add_argument('--origin',      default="-111", help="Port where the cruise originates.")
+    parser.add_argument('--arrival',     default="-111", help="Port where the cruise ends.")
+    parser.add_argument('--region',      default="-111", help="The region you want to sail.")
+    parser.add_argument('--domestic',    default="true", help="If it is a domestic or international cruise.")
+    parser.add_argument('--cruise_line', default="0",    help="The cruise line you want to sail on.")
+    parser.add_argument('--ship',        default="-111", help="The cruise ship you want to sail on.")
 
     # Month & Year must be provided together
     arg_group = parser.add_argument_group('Required Together')
-    arg_group.add_argument('--month', type=int, default=-111, help="The month you want to sail. Jan is 1, Feb is 2, etc.")
-    arg_group.add_argument('--year', type=int, default=-111, help="The year you want to sail on (e.g., 2025, 2026).")
+    parser.add_argument('--month',       default="-111", help="The month (1-12).")
+    parser.add_argument('--year',        default="-111", help="The year (e.g., 2025).")
 
     # Create a mutually exclusive group
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-d', '--days', type=int, help="The number of single days you want to cruise.")
-    group.add_argument('-r', '--range', type=validate_range, help="The range of days you want to travel (e.g., 3-7).")
+    group.add_argument('-r', '--range', type=validate_range,        help="The range of days you want to travel (e.g., 3-7).")
 
     # Parse arguments
     args = parser.parse_args()
@@ -75,32 +77,37 @@ if __name__ == "__main__":
         parser.error("Both --month and --year must be provided together.")
 
     # Extract argument values
-    origin, arrival, region, cruise_line, ship, month, year = (
-        args.origin, args.arrival, args.region, args.cruise_line, args.ship, args.month, args.year
+    origin, arrival, region, cruise_line, ship, month, year, domestic = (
+        args.origin, args.arrival, args.region, args.cruise_line, args.ship,\
+        args.month, args.year, args.domestic
     )
 
-    cruise_line = get_mapped_value(args.cruise_line, CRUISE_LINES, "cruise line")
-    ship = get_mapped_value(args.ship, SHIPS, "ship")
-    origin = get_mapped_value(args.origin, PORTS, "origin")
-    arrival = get_mapped_value(args.arrival, PORTS, "arrival")
+    cruise_line   = get_mapped_value(args.cruise_line, CRUISE_LINES, "cruise line")
+    ship          = get_mapped_value(args.ship, SHIPS, "ship")
+    origin        = get_mapped_value(args.origin, PORTS, "origin")
+    arrival       = get_mapped_value(args.arrival, PORTS, "arrival")
     mapped_region = get_mapped_value(args.region, REGIONS, "region")
+
+    # If domestic is not true or false
+    if "true" not in domestic and "false" not in domestic:
+        print(f"I'm here {domestic=} {type(domestic)}")
+        exit()
 
     # If we have chosen specific days or a day range
     if args.days:
-        days = f"{args.days}/{args.days}"
+        min_days = max_days = str(args.days)
+        
     elif args.range:
         min_days, max_days = map(int, args.range.split("-"))
         # We can't have 0 as the minimum amount of days because we get no results.
         if min_days == 0:
             min_days = 1
-            
-        days = f"{max_days}/{min_days}"
         
 
 
     # Build the URL
-    ozcruising_url = f"https://www.ozcruising.com.au/searchcruise/bysearchbar/{cruise_line}/{month}/{year}/{origin}/true/{days}/{mapped_region}/{ship}/{region.title()}/{arrival}"
-
+    ozcruising_url = f"https://www.ozcruising.com.au/searchcruise/executesearch"
+    
     # Setup request session
     POST_URL = "https://www.ozcruising.com.au/cruisecardapi/renderforpage"
     HEADERS = {
@@ -110,19 +117,35 @@ if __name__ == "__main__":
         "X-Requested-With": "XMLHttpRequest"
     }
 
+    # Build the array for data we will send
+    cruise_data = {
+        "ArrivalOzPlaceId":      arrival,
+        "DepartureMonthYear":    year,
+        "DepartureMonthNumber":  month,
+        "Brand":                 cruise_line,           # 0 for 'All'
+        "MaxNights":             max_days,      # Based on your HTML snippet
+        "MinNights":             min_days,
+        "RegionHierarchyItemId": region, # Asia - South East Asia
+        "DomesticDepartures":    domestic,
+        "DepartureOzPlaceId":    origin,
+        "ShipId":                ship
+    }
+
     session = requests.session()
     session.headers.update(HEADERS)
-    response = session.get(ozcruising_url)
-
-    # Check response validity
-    if "data-oz-cruise-codes=" not in response.text:
-        print("No cruises found or unexpected response format.")
-        exit(1)
+    # Get the cruise codes JSON
+    response = session.post(ozcruising_url, data=cruise_data)
+    # If we have an error, print and then quit
+    if response.status_code != 200:
+        print("[!] Error: An error occured after the post request.")
+        exit()
 
     # Extract cruise codes
-    cruise_codes = response.text.split("data-oz-cruise-codes=")[1].split("\"")[1].split(",")
+    cruise_codes = response.json()['result']
     print(f"Found {len(cruise_codes)} cruises")
 
+    if len(cruise_codes) == 0:
+        exit()
     # Fetch cruise details
     cruises = fetch_cruises_in_batches(cruise_codes, session, POST_URL, HEADERS)
 
@@ -140,5 +163,7 @@ if __name__ == "__main__":
         for item in cruises
     ]
     
-    with open(f"{DIR}/cruises.json", "w") as f:
+    date = datetime.now().strftime("%Y_%m_%d")
+    with open(f"{DIR}/cruises_{date}.json", "w") as f:
+        print(f"[!] Dumping {len(cruises)} cruises to a {DIR}/cruises_{date}.json")
         json.dump(formatted_data, f, indent=2)
